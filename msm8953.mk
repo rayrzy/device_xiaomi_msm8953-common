e4c23648fc06a3e0f8f2d475d74bfca181c0e7ab
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    msm8953-common: add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Change-Id: I6bc9196c0ef0aecb49f719a35c53e382a2c6ed2b
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     GrapheneCamera
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
