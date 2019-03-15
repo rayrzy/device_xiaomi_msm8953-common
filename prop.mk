bc9ae300ba6eb26ad72da8bb72d4f03f62e53af3
Lucas Dupin <dupin@google.com>
2019-03-14 17:04:05 -0700

    msm8953-common: Kang walleye offsets for GPU composition
    Window animations now have rounded corners, meaning that
    the shader that we use is more expensive than it used to be.
    
    Because of this, we need to adjust the SF offsets, to make sure
    that we'll have enough time to finish the GPU composition without
    dropping frames.
    
    These offsets as similar to blueline's, adjusted slightly because
    we were getting false "frame missed" signals if composition
    starts too early.
    
    Change-Id: I0e97ab54264292597812d6dfbeb0df989123944b
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- prop.mk
+++ prop.mk
@@ -115,6 +115,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.qti.va_aosp.support=1 \
 ro.vendor.qti.va_odm.support=1
 
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_phase_offset_ns=1500000
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
