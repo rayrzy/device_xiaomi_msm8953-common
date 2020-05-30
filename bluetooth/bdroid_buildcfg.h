6e2f530a96e3211bbc9363a0977c635335c2e77e
Nick C <ctwoon@suicide.today>
2020-05-30 12:50:04 +0300

    msm8953-common: update bdroid_buildcfg values from caf
    https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf/tree/build/qva/config/bdroid_buildcfg.h?h=LA.UM.8.6.2.r1-08400-89xx.0
    
    Change-Id: I3e7be287968b05fe3847214a0dd07677117f0471
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,14 +18,14 @@
 #define _BDROID_BUILDCFG_H
 
 #define MAX_ACL_CONNECTIONS    16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
+#define AVDT_NUM_SEPS 35
 
 #define BTIF_HF_WBS_PREFERRED TRUE
 #endif
