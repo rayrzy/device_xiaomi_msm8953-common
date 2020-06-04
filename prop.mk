94d224ed9f360641f9ea3b86e0b1a1218bb266fb
KenHV <kenhv@null.net>
2020-06-04 17:59:02 +0530

    msm8953-common: enable eis
    Change-Id: I152a1473a4ee9eac902493dae58bbdf9b2ae4d58
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- prop.mk
+++ prop.mk
@@ -75,7 +75,8 @@ persist.vendor.camera.display.umax=1920x1080 \
 persist.vendor.camera.display.lmax=1280x720 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1
 
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
